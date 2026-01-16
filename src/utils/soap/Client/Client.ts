dac7e4d37e6ef05e01e5cdc45dad9997d7963bf1
Jonathan <jonathanshapiro789@gmail.com>
2026-01-15 21:07:51 -0500

    remove api key gen

--- src/utils/soap/Client/Client.ts
+++ src/utils/soap/Client/Client.ts
@@ -15,7 +15,7 @@ export default class Client {
   private __username__: string;
   private __password__: string;
   private __district__: string;
-  private __apiKey__ : string;
+ 
   private static url:string;
   private readonly isParent: number;
   encrypted: boolean;
@@ -32,9 +32,7 @@ export default class Client {
     return this.__password__;
   }
 
-  public get apiKey():string{
-    return this.__apiKey__
-  }
+ 
 
   public get proxyUrl():string{
     return Client.url
@@ -53,7 +51,7 @@ export default class Client {
     this.__username__ = credentials.username;
     this.__password__ = credentials.password;
     this.__district__ = credentials.districtUrl;
-    this.__apiKey__ = generateKey()
+ 
 
     this.isParent = credentials.isParent ? 1 : 0;
     this.encrypted=credentials.encrypted;
@@ -251,42 +249,3 @@ export default class Client {
 }
 
 
-
-function getDateMMDDYY() {
-  let date = new Date();
-  const epoch=date.getTime();
-  date=new Date(epoch)
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so +1
-  const day = String(date.getDate()).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${month}${day}${year}`;
-}
-
-function generateKey() : string {
-  try {
-    // Prepare key and IV
-    let keyBytes = CryptoJS.enc.Utf8.parse('b2524efb438b4532b322e633d5aff252');  // Convert key to a word array
-    let ivBytes = CryptoJS.enc.Utf8.parse('AES');  // Convert IV to a word array
-
-
-    // Define the input string (date, version, etc.)
-    const today = getDateMMDDYY();
-    let input = `${today}|9.0.0|${today}|android`;
-
-    // Encrypt the input string using AES with CBC mode and PKCS7 padding
-    let encrypted = CryptoJS.AES.encrypt(input, keyBytes, {
-      iv: ivBytes,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7
-    });
-
-    // Convert the encrypted data to a Base64 string
-    let encryptedString = encrypted.toString();
-
-    return encryptedString;
-  } catch (error:any) {
-    console.error(error);
-    return error.message
-  }
-}
